744c2994bad5416f34a68eedfe840911bb58c4f6
EATSTEAK <me@eatsteak.dev>
2023-11-26 18:40:19 +0900

    feat: responsive layout support for post pages

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,17 @@
 import "./header.css";
 
-export const Header = (props) => {
-  const selected = (page) =>
+export const Header = (props: { page: string }) => {
+  const selected = (page: string) =>
     props.page == page ? "nav-link link-selected" : "nav-link";
   return (
     <header class="flex justify-center content-center px-6 py-6">
-      <div class="grow max-w-[96rem] flex gap-16 justify-start content-center">
-        <h2 class="shrink-0 min-w-fit">
+      <div class="grow max-w-[96rem] flex flex-wrap gap-4 lg:gap-16 justify-between lg:justify-start content-center">
+        <h2 class="shrink-0 min-w-fit order-1">
           <a href="/">
             <img src="/logo.png" class="w-[170px]"></img>
           </a>
         </h2>
-        <nav class="grow flex justify-start items-center gap-8">
+        <nav class="grow flex justify-center lg:justify-start items-center gap-8 order-3 lg:order-2 basis-full lg:basis-1">
           <a class={selected("about")} href="/about">
             About
           </a>
@@ -22,7 +22,7 @@ export const Header = (props) => {
             Categories
           </a>
         </nav>
-        <aside class="shrink-0 flex justify-end items-center gap-2">
+        <aside class="shrink-0 flex justify-end items-center gap-2 order-2 lg:order-3">
           <input type="checkbox"></input>
           <span class="align-center">LIGHT</span>
         </aside>
