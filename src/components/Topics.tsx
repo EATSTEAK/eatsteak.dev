e3ec5ffd031fd53c99afd9e49957a208285ca37c
EATSTEAK <me@eatsteak.dev>
2023-12-09 13:33:14 +0900

    refactor: rename Tags to Topics

--- src/components/Topics.tsx
+++ src/components/Topics.tsx
@@ -4,9 +4,9 @@ export const Topics: Component<{ topics: string[] }> = (props) => {
   return (
     <div class="flex gap-2 flex-wrap">
       <For each={props.topics}>
-        {(t, i) => (
+        {(topic, i) => (
           <div class="font-mono uppercase cursor-pointer transition-colors border-gray-700 dark:border-white border px-1">
-            {tag}
+            {topic}
           </div>
         )}
       </For>
