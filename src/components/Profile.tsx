3a6fa456534de0836f16b6db87322a6cdd1f515d
EATSTEAK <me@eatsteak.dev>
2023-12-09 12:57:12 +0900

    feat: profile design

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,3 +1,29 @@
+import { GitHub } from "../icons/GitHub";
+import { Instagram } from "../icons/Instagram";
+
 export const Profile = () => {
-    return (<section>EATSTEAK</section>)
-}
+  return (
+    <section>
+      <div class="flex gap-2 items-center mb-2">
+        <div class="bg-gray-200 rounded-full w-16 h-16">
+          <img src="/images/profile.png" class="w-16"></img>
+        </div>
+        <div class="grow">
+          <h5 class="m-0">EATSTEAK</h5>
+          <p class="m-0">전 취미로 코딩해요</p>
+        </div>
+      </div>
+      <div>
+        <h5>FOLLOW ME AT</h5>
+        <div class="flex gap-4 items-center">
+          <a href="https://github.com/eatsteak">
+            <GitHub class="w-8 hover:text-gray-900 text-gray-700 dark:text-gray-300 dark:hover:text-gray-100 transition-colors" />
+          </a>
+          <a href="https://instagram.com/eatsteak.dev">
+            <Instagram class="w-8 hover:text-gray-900 text-gray-700 dark:text-gray-300 dark:hover:text-gray-100 transition-colors" />
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
