5f925e76ec01a0a0d041d559e0d098ed2323f042
EATSTEAK <me@eatsteak.dev>
2023-12-09 13:30:53 +0900

    fix: flex-wrap in tags

--- src/components/Tags.tsx
+++ src/components/Tags.tsx
@@ -2,7 +2,7 @@ import { type JSX, type Component, For } from "solid-js";
 
 export const Tags: Component<{ tags: string[] }> = (props) => {
   return (
-    <div class="flex gap-2">
+    <div class="flex gap-2 flex-wrap">
       <For each={props.tags}>
         {(tag, i) => (
           <div class="font-mono uppercase cursor-pointer transition-colors border-gray-700 dark:border-white border px-1">
