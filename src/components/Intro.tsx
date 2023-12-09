e34c550504027c25ac68116a0c0d163700f24397
EATSTEAK <me@eatsteak.dev>
2023-12-09 15:50:32 +0900

    design: apply animation in intro

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -10,7 +10,7 @@ export const Intro = () => {
           </div>
         </div>
         <div class="grill-background before:bg-slate-700 dark:before:bg-slate-400 before:bg-opacity-30 before:rotate-1 w-full h-full">
-          <div class="py-8 px-12 lg:mx-auto h-full w-full max-w-[96rem] flex flex-col items-start justify-end gap-2">
+          <div class="slide-to-top py-8 px-12 lg:mx-auto h-full w-full max-w-[96rem] flex flex-col items-start justify-end gap-2">
             <p
               class="py-2 px-2 m-0 font-head font-bold text-5xl bg-gray-800 text-white
             dark:bg-white dark:text-gray-800"
@@ -25,8 +25,8 @@ export const Intro = () => {
             </p>
             <a
               class="py-2 px-4 transition-colors border-2
-              bg-white hover:bg-gray-800 text-gray-800 hover:text-white border-gray-800
-              dark:bg-gray-800 dark:hover:bg-white dark:text-white dark:hover:text-gray-800 dark:border-white"
+              before:bg-white bg-gradient-to-r hover-fill-to-right from-gray-800 to-gray-800 text-gray-800 hover:text-white border-gray-800
+               dark:before:bg-gray-800 dark:from-white dark:to-white dark:text-white dark:hover:text-gray-800 dark:border-white"
               href="/about"
             >
               더 알아보기
