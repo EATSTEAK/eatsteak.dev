7a0adcf3396a59b5154161595deeb44ed6f86553
EATSTEAK <me@eatsteak.dev>
2023-12-16 19:49:11 +0900

    design: rotate background 2deg more

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -10,7 +10,7 @@ export const Intro: Component<{ recentPosts: Flatten<ContentEntryMap["blog"]>}> 
             <AnimatedLogo class="-mt-16 w-[32rem] h-[32rem]" />
           </div>
         </div>
-        <div class="grill-background before:bg-slate-700 dark:before:bg-slate-400 before:bg-opacity-30 before:rotate-1 w-full h-full">
+        <div class="grill-background before:bg-slate-700 dark:before:bg-slate-400 before:bg-opacity-30 before:rotate-3 w-full h-full">
           <div class="slide-to-top py-8 px-12 lg:mx-auto h-full w-full max-w-[96rem] flex flex-col items-start justify-end gap-2">
             <p
               class="py-2 px-2 m-0 font-head font-bold text-5xl bg-gray-800 text-white
