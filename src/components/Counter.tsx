5ec3be06d85ea9da99ce831ac5cd4925a3452ed3
EATSTEAK <me@eatsteak.dev>
2024-01-28 15:32:50 +0900

    fix: counter in web pages when path has trailing slash

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -6,8 +6,9 @@ import {
 } from "solid-js";
 
 const fetchCount = async (path: string): Promise<number> => {
+  const normalizedPath = path.endsWith("/") ? path.substring(0, path.length - 1) : path;
   let json = (await (
-    await fetch(`https://eatsteak.goatcounter.com/counter/${path}.json`)
+    await fetch(`https://eatsteak.goatcounter.com/counter/${normalizedPath}.json`)
   ).json()) as { count: string; count_unique: string };
   return parseInt(json.count);
 };
