36f2935a4baa7fa97bc6926bd90967400d7fa855
Hyomin Koo <me@eatsteak.dev>
2024-06-08 04:01:01 +0000

    fix: update copyright comment to cover currentYear

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,11 @@
 import type { JSX, Component } from "solid-js";
 
 export const Footer: Component<{}> = () => {
+  const currentYear = new Date().getFullYear();
   return (
     <footer class="w-full flex justify-center py-12">
-      <span>Copyright 2023 <a href="https://eatsteak.dev">EATSTEAK</a> All rights
-      reserved.</span>
+      <span>Copyright 2023-{currentYear} <a href="https://eatsteak.dev">EATSTEAK</a>. All rights
+        reserved.</span>
     </footer>
   );
 };
