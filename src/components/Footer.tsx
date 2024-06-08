f87f4cf710268168980a18828d6e311e5cb4c5ce
Hyomin Koo <me@eatsteak.dev>
2024-06-08 07:18:17 +0000

    style: change footer font to `font-mono`

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export const Footer: Component<{}> = () => {
   const currentYear = new Date().getFullYear();
   return (
     <footer class="w-full flex justify-center py-12">
-      <span>Copyright 2023-{currentYear} <a href="https://eatsteak.dev">EATSTEAK</a>. All rights
+      <span class="font-mono">Copyright 2023-{currentYear} <a href="https://eatsteak.dev">EATSTEAK</a>. All rights
         reserved.</span>
     </footer>
   );
