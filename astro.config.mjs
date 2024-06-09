c758faf02aa8be6000787a519a807b63962513d8
Hyomin Koo <me@eatsteak.dev>
2024-06-09 13:15:43 +0000

    feat: apply `astro-compress`

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import solidJs from "@astrojs/solid-js";
-
 import tailwind from "@astrojs/tailwind";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://eatsteak.dev",
-  integrations: [mdx(), sitemap(), solidJs(), tailwind()],
-  markdown: {
-  },
+  integrations: [mdx(), sitemap(), solidJs(), tailwind(), compress()],
+  markdown: {}
 });
