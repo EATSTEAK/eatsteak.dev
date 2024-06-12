eaa9d3b493fd5bb30301aa996c5d685843fe4155
Hyomin Koo <me@eatsteak.dev>
2024-06-12 10:12:01 +0000

    feat: toc indicator

---
+++ src/components/TableOfContents.tsx
@@ -0,0 +1,42 @@
+import { TocHeading } from "./TocHeading";
+import { type JSX, type Component, createSignal } from "solid-js";
+
+export const TableOfContents: Component<{ tocHeadings: TocHeading[] }> = (props) => {
+    const [activeSections, setActiveSections] = createSignal<string[]>([]);
+    const observer = new IntersectionObserver((sections) => {
+        let currentActiveSections = [...activeSections()];
+        let hasUpdate = false;
+        sections.forEach((section) => {
+
+            const heading = section.target.querySelector("h2, h3, h4, h5");
+            if (!heading) return;
+            const id = heading.getAttribute("id");
+            if (id == null) return;
+            if (section.intersectionRatio > 0) {
+                if(!currentActiveSections.includes(id)) {
+                    currentActiveSections.push(id);
+                    hasUpdate = true;
+                }
+            } else {
+                let index = currentActiveSections.indexOf(id);
+                if (index > -1) {
+                    currentActiveSections.splice(index, 1);
+                    hasUpdate = true;
+                }
+            }
+        })
+        if (hasUpdate) {
+            console.log(currentActiveSections);
+            setActiveSections(currentActiveSections);
+        }
+    });
+
+    document.querySelectorAll(".prose section").forEach((section) => {
+        observer.observe(section);
+    })
+    return (
+        <nav>
+            <TocHeading tocHeadings={props.tocHeadings} activeHeadings={activeSections()} />
+        </nav>
+    );
+};
--- src/components/TocHeading.tsx
+++ src/components/TocHeading.tsx
@@ -1,16 +1,18 @@
 import "./toc_heading.css";
-import { type JSX, type Component, For, Show } from "solid-js";
+import { type JSX, type Component, For, Show, createSignal } from "solid-js";
 
-export const TocHeading: Component<{ tocHeadings: TocHeading[] }> = (props) => {
+export const TocHeading: Component<{ tocHeadings: TocHeading[], activeHeadings: string[] }> = (props) => {
+    
+    
     return (
         <ul>
             <For each={props.tocHeadings}>
                 {(heading) => <li>
-                    <a class="nav-link" href={`#${heading.slug}`}>
+                    <a class={`nav-link ${props.activeHeadings.includes(heading.slug) ? "link-selected" : ""}`} href={`#${heading.slug}`}>
                         {heading.text}
                     </a>
                     <Show when={heading.subheadings.length > 0}>
-                        <TocHeading tocHeadings={heading.subheadings} />
+                        <TocHeading tocHeadings={heading.subheadings} activeHeadings={props.activeHeadings} />
                     </Show>
                 </li>}
             </For>
