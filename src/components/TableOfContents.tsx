6c371905bb7501e8c7f2c9f7896b5736ae2bfa78
Hyomin Koo <me@eatsteak.dev>
2024-06-24 03:19:02 +0000

    refactor: building toc with `ReactiveSet`

--- src/components/TableOfContents.tsx
+++ src/components/TableOfContents.tsx
@@ -1,35 +1,21 @@
+import { ReactiveSet } from "@solid-primitives/set";
 import { TocHeading } from "./TocHeading";
 import { type JSX, type Component, createSignal } from "solid-js";
 
 export const TableOfContents: Component<{ tocHeadings: TocHeading[] }> = (props) => {
-    const [activeSections, setActiveSections] = createSignal<string[]>([]);
-    const observer = new IntersectionObserver((sections) => {
-        let currentActiveSections = [...activeSections()];
-        let hasUpdate = false;
-        sections.forEach((section) => {
+    const activeSections = new ReactiveSet<string>([]);
+    const observer = new IntersectionObserver((sections) => sections.forEach((section) => {
 
-            const heading = section.target.querySelector("h2, h3, h4, h5");
-            if (!heading) return;
-            const id = heading.getAttribute("id");
-            if (id == null) return;
-            if (section.intersectionRatio > 0) {
-                if(!currentActiveSections.includes(id)) {
-                    currentActiveSections.push(id);
-                    hasUpdate = true;
-                }
-            } else {
-                let index = currentActiveSections.indexOf(id);
-                if (index > -1) {
-                    currentActiveSections.splice(index, 1);
-                    hasUpdate = true;
-                }
-            }
-        })
-        if (hasUpdate) {
-            console.log(currentActiveSections);
-            setActiveSections(currentActiveSections);
+        const heading = section.target.querySelector("h2, h3, h4, h5");
+        if (!heading) return;
+        const id = heading.getAttribute("id");
+        if (id == null) return;
+        if (section.intersectionRatio > 0) {
+            if (!activeSections.has(id)) activeSections.add(id);
+        } else {
+            if (activeSections.has(id)) activeSections.delete(id);
         }
-    });
+    }));
 
     document.querySelectorAll(".prose section").forEach((section) => {
         observer.observe(section);
@@ -37,7 +23,7 @@ export const TableOfContents: Component<{ tocHeadings: TocHeading[] }> = (props)
     return (
         <nav class="w-80">
             <p class="text-center font-head font-semibold">CONTENTS</p>
-            <TocHeading tocHeadings={props.tocHeadings} activeHeadings={activeSections()} />
+            <TocHeading tocHeadings={props.tocHeadings} activeHeadings={[...activeSections]} />
         </nav>
     );
 };
