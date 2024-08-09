f5fc55e6280cf0ab9e458617969e1e97136fc4cd
EATSTEAK <me@eatsteak.dev>
2024-08-09 12:14:29 +0900

    style: code indentation

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,26 +1,26 @@
-import type { JSX, Component } from "solid-js";
-import { AnimatedLogo } from "./AnimatedLogo";
-import { Button } from "./Button";
-import { GrillContainer } from "./GrillContainer";
+import type {JSX, Component} from "solid-js";
+import {AnimatedLogo} from "./AnimatedLogo";
+import {Button} from "./Button";
+import {GrillContainer} from "./GrillContainer";
 import "./intro.css";
 
 export const Intro: Component<{}> = () => {
-  return (
-    <GrillContainer absoluteBackground={<AnimatedLogo class="-mt-16 w-[32rem] h-[32rem]" />}>
-      <p
-            class="box-text"
-          >
-            Grilling
-          </p>
-          <p
-            class="box-text"
-          >
-            Burning-Hot Applications
-          </p>
-          <Button href="/about"
-          >
-            프로필 보기
-          </Button>
-    </GrillContainer>
-  );
+    return (
+        <GrillContainer absoluteBackground={<AnimatedLogo class="-mt-16 w-[32rem] h-[32rem]"/>}>
+            <p
+                class="box-text"
+            >
+                Grilling
+            </p>
+            <p
+                class="box-text"
+            >
+                Burning-Hot Applications
+            </p>
+            <Button href="/about"
+            >
+                프로필 보기
+            </Button>
+        </GrillContainer>
+    );
 };
