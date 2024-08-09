e40a27d46f325310058723cb59bdb44def90220f
EATSTEAK <me@eatsteak.dev>
2024-08-09 16:51:20 +0900

    fix(about): hidden texts in mobile

--- src/components/GrillContainer.tsx
+++ src/components/GrillContainer.tsx
@@ -1,4 +1,4 @@
-import { type JSX, type Component, Show } from "solid-js";
+import { type Component, type JSX, Show } from "solid-js";
 import "./grill_container.css";
 
 export const GrillContainer: Component<{
@@ -9,22 +9,19 @@ export const GrillContainer: Component<{
   containerClassOverride?: string;
 }> = (props) => {
   return (
-    <section class={props.parentClassOverride ?? "relative h-72"}>
-      <div class={`grill-background size-full ${props.grillClassOverride ?? "before:bg-slate-700 dark:before:bg-slate-400 before:bg-opacity-30 before:rotate-3"}`}>
+    <section class={props.parentClassOverride ?? "relative min-h-72"}>
+      <div
+        class={`grill-background size-full ${props.grillClassOverride ?? "before:bg-slate-700 dark:before:bg-slate-400 before:bg-opacity-30 before:rotate-3"}`}
+      >
         <div
-          class={
-            props.containerClassOverride ??
-            "slide-to-top grill-content"
-          }
+          class={props.containerClassOverride ?? "slide-to-top grill-content"}
         >
           {props.children}
         </div>
       </div>
       <Show when={props.absoluteBackground}>
         <div class="background-container">
-          <div class="background-element">
-            {props.absoluteBackground}
-          </div>
+          <div class="background-element">{props.absoluteBackground}</div>
         </div>
       </Show>
     </section>
