d64a1db5f8fb9a69e0a5bbf82b09843998e3a86e
EATSTEAK <me@eatsteak.dev>
2024-08-20 09:31:15 +0900

    feat: comments section powered by `utterances`

---
+++ src/components/Comments.tsx
@@ -0,0 +1,28 @@
+import { type Component, createEffect } from "solid-js";
+import theme from "../utils/theme.ts";
+
+export const Comments: Component<{}> = () => {
+  const [isDarkTheme, _] = theme;
+  let sectionRef: HTMLDivElement;
+  const utterances = () => {
+    const utterancesScript = document.createElement("script");
+    utterancesScript.src = "https://utteranc.es/client.js";
+    utterancesScript.setAttribute("repo", "eatsteak/eatsteak.dev");
+    utterancesScript.setAttribute("issue-term", "pathname");
+    utterancesScript.setAttribute("label", "blog");
+    utterancesScript.setAttribute(
+      "theme",
+      isDarkTheme() ? "dark-blue" : "github-light",
+    );
+    utterancesScript.crossOrigin = "anonymous";
+    utterancesScript.async = true;
+    return utterancesScript;
+  };
+  createEffect(() => {
+    if (sectionRef) {
+      sectionRef.innerHTML = "";
+      sectionRef.appendChild(utterances());
+    }
+  }, [isDarkTheme]);
+  return <section class="comments" ref={sectionRef}></section>;
+};
