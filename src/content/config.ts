b11a5182eefae263a2002ecb265a1d976ad2f52e
Hyomin Koo <hyomin@hyperithm.com>
2024-11-28 10:09:17 +0900

    feat: basic slide generation

--- src/content/config.ts
+++ src/content/config.ts
@@ -16,4 +16,19 @@ const blog = defineCollection({
   }),
 });
 
-export const collections = { blog };
+const slide = defineCollection({
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    category: z.string().optional(),
+    topics: z.array(z.string()).optional(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    hidden: z.boolean().optional(),
+    series: z.string().optional(),
+  }),
+});
+
+export const collections = { blog, slide };
