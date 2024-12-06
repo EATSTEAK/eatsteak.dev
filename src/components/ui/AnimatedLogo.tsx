67c0a547693e1e4cd55fbe1131a4074275d037c2
Hyomin Koo <me@eatsteak.dev>
2024-12-06 16:16:16 +0900

    fix: logo disappearing in light mode

--- src/components/ui/AnimatedLogo.tsx
+++ src/components/ui/AnimatedLogo.tsx
@@ -1,10 +1,11 @@
 import type { JSX, Component } from "solid-js";
 import { Profile } from "@icons/Profile";
+import { cn } from "@utils/cn";
 export const AnimatedLogo: Component<JSX.HTMLAttributes<HTMLDivElement>> = (
   props
 ) => {
   return (
-    <div {...props} class={`${props.class ?? ""}`} role="presentation">
+    <div {...props} class={cn(props.class)} role="presentation">
       <Profile />
     </div>
   );
--- src/icons/Profile.tsx
+++ src/icons/Profile.tsx
@@ -10,6 +10,7 @@ export const Profile: Component<JSX.SvgSVGAttributes<SVGSVGElement>> = (
       height="512"
       viewBox="0 0 512 512"
       fill="currentColor"
+      color="white"
       {...props}
       xmlns="http://www.w3.org/2000/svg"
     >
