fbd7c9268f4b6a9907b8a8a0e5dab67f8458057b
EATSTEAK <me@eatsteak.dev>
2024-12-07 22:59:07 +0900

    feat: no class overriding with cn

--- src/components/shell/GrillContainer.tsx
+++ src/components/shell/GrillContainer.tsx
@@ -1,24 +1,26 @@
 import { type Component, type JSX, Show } from "solid-js";
 import "./grill_container.css";
+import { cn } from "@utils/cn.ts";
 
 export const GrillContainer: Component<{
   absoluteBackground?: JSX.Element;
   children: JSX.Element;
-  parentClassOverride?: string;
-  grillClassOverride?: string;
-  containerClassOverride?: string;
+  parentClass?: string;
+  grillClass?: string;
+  containerClass?: string;
 }> = (props) => {
   return (
-    <section class={props.parentClassOverride ?? "relative min-h-72"}>
+    <section class={cn("relative overflow-hidden min-h-72", props.parentClass)}>
       <div
-        class={`grill-background size-full ${props.grillClassOverride ?? "before:bg-slate-700 dark:before:bg-slate-400 before:bg-opacity-30 before:rotate-3"}`}
-      >
-        <div
-          class={props.containerClassOverride ?? "slide-to-top grill-content"}
-        >
-          {props.children}
-        </div>
+        class={cn(
+          "grill-background bg-slate-700 dark:bg-slate-400 bg-opacity-30 rotate-3",
+          props.grillClass,
+        )}
+      ></div>
+      <div class={cn("slide-to-top grill-content", props.containerClass)}>
+        {props.children}
       </div>
+
       <Show when={props.absoluteBackground}>
         <div class="background-container">
           <div class="background-element">{props.absoluteBackground}</div>
