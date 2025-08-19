0f9ed97bb2591448904ea03511da97dd57529e1f
EATSTEAK <me@eatsteak.dev>
2025-08-19 23:32:07 +0900

    chore: migrate stylelint configuration

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,2 +1,25 @@
 /** @type {import('stylelint').Config} */
-export default {};
+export default {
+  extends: ["stylelint-config-standard", "stylelint-config-tailwindcss"],
+  rules: {
+    "at-rule-no-unknown": [
+      true,
+      {
+        ignoreAtRules: [
+          "utility",
+          "apply",
+          "theme",
+          "custom-variant",
+          "reference",
+          "plugin",
+        ],
+      },
+    ],
+    "at-rule-no-deprecated": [
+      true,
+      {
+        ignoreAtRules: ["apply"],
+      },
+    ],
+  },
+};
